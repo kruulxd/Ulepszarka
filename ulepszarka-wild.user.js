4677dd9b467cfa02b04e923669727c165e81445a
kruulxd <56634090+kruulxd@users.noreply.github.com>
2026-03-25 14:23:45 +0100

    Update ulepszarka-wild.user.js

--- ulepszarka-wild.user.js
+++ ulepszarka-wild.user.js
@@ -1,27 +1,14 @@
 // ==UserScript==
 // @name         ulepszator by Kruul
-// @namespace    http://tampermonkey.net/
-// @version      0.2
-// @description  Auto ulepszanie i rozbijanie
-// @author       Kruul
-// @match        https://*.margonem.pl/
-// @updateURL    https://raw.githubusercontent.com/kruulxd/Ulepszarka/main/ulepszarka-wild.user.js
-// @downloadURL  https://raw.githubusercontent.com/kruulxd/Ulepszarka/main/ulepszarka-wild.user.js
+// @version      0.1.8
 // @grant        GM_setValue
 // @grant        GM_getValue
 // @grant        GM_deleteValue
 // @grant        GM_listValues
 // ==/UserScript==
 
-'use strict';
-(function () {
-  document.ulepszarka = {
-    GM_getValue,
-    GM_setValue,
-    GM_deleteValue,
-    GM_listValues
-  };
-
+(function() {
+  document.ulepszarka = { GM_getValue, GM_setValue, GM_deleteValue, GM_listValues };
   const script = document.createElement('script');
   script.src = 'https://raw.githubusercontent.com/kruulxd/Ulepszarka/main/ulepszarka-bundle.js?v=' + Date.now();
   document.body.appendChild(script);
